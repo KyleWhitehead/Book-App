bcc6736fd4b3ca91a908d5c77191396b26e2fc80
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-07-04 15:00:10 -0600

    Intstalled the correct packages for Font Awesome and still working on features

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,8 +1,32 @@
+import React from "react";
+import "./Features.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
+
 const Features = () => {
   return (
-    <div>
-        
-    </div>
+    <>
+      <section id="features">
+        <div className="container">
+          <div className="row">
+            <div className="section__title">Understand books in a few minutes</div>
+            <div className="features__wrapper">
+              <div className="features">
+                <div className="features__icon">
+                  <FontAwesomeIcon icon={faFile} />
+                </div>
+                <div className="features__title">Read or Listen</div>
+                <div className="features__sub--title">
+                  Save time by getting core ideas from the best books
+                </div>
+              </div>
+              <div className="features"></div>
+              <div className="features"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,11 @@
 const Footer = () => {
   return (
-    <div>
-        
-    </div>
+    <footer className="footer">
+      <div className="footer__content">
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
+      </div>
+    </footer>
   );
-};
+}
 
 export default Footer;
