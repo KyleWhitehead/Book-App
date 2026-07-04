943d8aac4f6b6c8ad39095a394e17618ba8c09bd
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-07-04 11:40:32 -0600

    Routing Fixes for Milestone 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import Book from './Pages/Book/Book.jsx';
 import ChoosePlan from './Pages/ChoosePlan/ChoosePlan';
 import ForYou from './Pages/ForYou/ForYou';
@@ -6,14 +5,23 @@ import Home from './Pages/Home/Home';
 import Library from './Pages/Library/Library';
 import Player from './Pages/Player/Player';
 import Settings from './Pages/Settings/Settings';
-import SideBar from './components/Sidebar/SideBar';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to My React App</h1>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/book" element={<Book />} />
+          <Route path="/choose-plan" element={<ChoosePlan />} />
+          <Route path="/for-you" element={<ForYou />} />
+          <Route path="/library" element={<Library />} />
+          <Route path="/player" element={<Player />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
