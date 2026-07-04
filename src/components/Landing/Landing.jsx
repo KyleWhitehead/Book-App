379d2b404c9737c565e925f4b7cfea63037a611a
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-07-04 13:47:47 -0600

    Landing Page Updates

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,8 +1,26 @@
+import "./Landing.css";
+import landing from "../../assets/landing.png";
+
 const Landing = () => {
   return (
-    <div>
-        
-    </div>
+    <section id="landing">
+      <div className="row">
+        <div className="landing__wrapper">
+            <div className="landing__content__title">
+                Gain more knowledge in less time
+            </div>
+            <div className="landing__content__subtitle">
+                Great summaries for busy people,
+                Idviduals who have barely anytime to read,
+                and even people who don't like to read.
+            </div>
+            <button className="btn home__cta--btn">Login</button>
+        </div>
+        <figure className="landing__image--mask">
+            <img src={landing} alt="Landing" />
+        </figure>
+      </div>
+    </section>
   );
 };
 
