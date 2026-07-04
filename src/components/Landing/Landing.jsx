f460b625cc096675b0718c329eaa990d533ea4c1
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-07-04 14:17:47 -0600

    Landing page Updates to HTML and CSS

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -6,19 +6,20 @@ const Landing = () => {
     <section id="landing">
       <div className="row">
         <div className="landing__wrapper">
+          <div className="landing__description">
             <div className="landing__content__title">
-                Gain more knowledge in less time
+              Gain more knowledge in less time
             </div>
             <div className="landing__content__subtitle">
-                Great summaries for busy people,
-                Idviduals who have barely anytime to read,
-                and even people who don't like to read.
+              Great summaries for busy people, Idviduals who have barely anytime
+              to read, and even people who don't like to read.
             </div>
             <button className="btn home__cta--btn">Login</button>
-        </div>
-        <figure className="landing__image--mask">
+          </div>
+          <figure className="landing__image--mask">
             <img src={landing} alt="Landing" />
-        </figure>
+          </figure>
+        </div>
       </div>
     </section>
   );
