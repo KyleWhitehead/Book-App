3c4c5381ca3034beb838d1b1065f1c2c02467184
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-07-04 11:09:53 -0600

    part 1 of Milestone 2

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,20 @@
+import React from "react";
+import Features from "../../components/Features/Features";
+import Footer from "../../components/Footer/Footer";
+import HomeNavbar from "../../components/HomeNavbar/HomeNavbar";
+import Landing from "../../components/Landing/Landing";
+import Numbers from "../../components/Numbers/Numbers";
+import Reviews from "../../components/Reviews/Reviews";
+
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <HomeNavbar />
+      <Landing />
+      <Features />
+      <Footer />
+      <Numbers />
+      <Reviews />
     </div>
   );
 };
---
+++ src/components/HomeNavbar/HomeNavbar.jsx
@@ -0,0 +1,16 @@
+const HomeNavbar = () => {
+  return (
+    <nav>
+      <h2>Summarist</h2>
+
+      <ul>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Help</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default HomeNavbar;
