784021d42baa92d4950040091a4389083557221c
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-07-10 19:35:47 -0600

    Finsished editing Features component to include required look and functionality.

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Features.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFile } from "@fortawesome/free-solid-svg-icons";
+import { faFile, faHeadphones, faLightbulb } from "@fortawesome/free-solid-svg-icons";
 
 const Features = () => {
   return (
@@ -20,8 +20,24 @@ const Features = () => {
                   Save time by getting core ideas from the best books
                 </div>
               </div>
-              <div className="features"></div>
-              <div className="features"></div>
+              <div className="features">
+                <div className="features__icon">
+                  <FontAwesomeIcon icon={faLightbulb} />
+                </div>
+                <div className="features__title">Find your next read</div>
+                <div className="features__sub--title">
+                  Explore book lists and personalized recommendations.
+                </div>
+              </div>
+              <div className="features">
+                <div className="features__icon">
+                  <FontAwesomeIcon icon={faHeadphones} />
+                </div>
+                <div className="features__title">Briefcasts</div>
+                <div className="features__sub--title">
+                  Gain valuable insights from briefcasts
+                </div>
+              </div>
             </div>
           </div>
         </div>
