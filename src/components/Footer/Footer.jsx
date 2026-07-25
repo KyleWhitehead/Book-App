3baaaaf04eaeb0da6cf6a09cc6d9df24658ba1be
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-07-25 12:04:47 -0600

    Changed Footer for now Updated Reviews component with CSS file

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__content">
-        <p>&copy; 2024 Your Company. All rights reserved.</p>
-      </div>
+      
     </footer>
   );
 }
